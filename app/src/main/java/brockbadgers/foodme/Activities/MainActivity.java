7d635d7877d079260091b887e0f9d865541276d7
Peter Wilson <pw12nb@brocku.ca>
2016-08-01 21:55:57 -0400

    add search bar instead of the action bar

--- app/src/main/java/brockbadgers/foodme/Activities/MainActivity.java
+++ app/src/main/java/brockbadgers/foodme/Activities/MainActivity.java
@@ -1,13 +1,18 @@
 package brockbadgers.foodme.Activities;
 
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.SearchView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
 
 import brockbadgers.foodme.R;
 
@@ -17,23 +22,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //change the action bar to a search bar using defined layout
+        ActionBar searchBar = getSupportActionBar();
+        searchBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.white)));
+        searchBar.setDisplayShowHomeEnabled(false);
+        searchBar.setDisplayShowTitleEnabled(false);
+        LayoutInflater searchInflater = LayoutInflater.from(this);
+
+        View searchView = searchInflater.inflate(R.layout.search_actionbar, null);
+
+        final SearchView search = (SearchView) searchView.findViewById(R.id.searchBar);
+
+        //implement the search functionality in the action bar
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onQueryTextChange(String query) {
+                //we arent searching on each keystroke so we wont be
+                //doing anything in this function's implementation
+                return true;
             }
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //we will query the Yelp API asychronously
+                //while it is searching we can pop up a loading screen
+                //TODO: search the YELP API
+
+                //Hide the keyboard
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(search.getWindowToken(), 0);
+                return true;
+
+            }
+
         });
+
+
+        searchBar.setCustomView(searchView);
+        searchBar.setDisplayShowCustomEnabled(true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
